50cc061062453cfbcaae35011ac44d99d1ba18b6
SkateZen <mariusmdm123@gmail.com>
2023-02-22 09:09:42 +0100

    Initial commit

---
+++ Assets/Scripts/ScenesLoader.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator LoadBirdCoroutine()
+    {
+        AsyncOperation asyncload = SceneManager.LoadSceneAsync("FurapiBirdLoad");
+        while (!asyncload.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void LoadBird()
+    {
+        StartCoroutine(LoadBirdCoroutine());    
+    }
+
+    public IEnumerator LoadAppleCoroutine()
+    {
+        AsyncOperation asyncload = SceneManager.LoadSceneAsync("AppleCatcherLoad");
+        while (!asyncload.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void LoadApple()
+    {
+        StartCoroutine(LoadAppleCoroutine());
+    }
+
+    public IEnumerator LoadBrickCoroutine()
+    {
+        AsyncOperation asyncload = SceneManager.LoadSceneAsync("BrickBreaker");
+        while (!asyncload.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void LoadBrick()
+    {
+        StartCoroutine(LoadBrickCoroutine());
+    }
+
+    public IEnumerator LoadStackCoroutine()
+    {
+        AsyncOperation asyncload = SceneManager.LoadSceneAsync("Stack");
+        while (!asyncload.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void LoadStack()
+    {
+        StartCoroutine(LoadStackCoroutine());
+    }
+
+
+    public void AppQuit()
+    {
+        Application.Quit();
+    }
+}
